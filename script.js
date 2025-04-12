4342bdeab9d1b063d2dbdefb29c8fc92314fa7b0
jashvanth370 <jashjazz11@gmail.com>
2025-04-12 09:19:39 +0530

    Full Code Updates

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let runningTotal=0;
 let buffer ="0";
 let previousOperator;
 
-const screen = document.querySelector('.scrren');
+const screen = document.querySelector('.screen');
+
 
 
 function buttonClick(value){
@@ -11,8 +12,9 @@ function buttonClick(value){
     }else{
         handleNumber(value);
     }
-    screen.innerText = buffer;
+    screen.innerText=buffer;
 }
+
 
 function handleSymbol(symbol){
     switch(symbol){
@@ -22,7 +24,7 @@ function handleSymbol(symbol){
             break;
 
         case '=':
-            if(previousOperator===Null){
+            if(previousOperator===null){
                 return 
             }
             flushOperation(parseInt(buffer));
@@ -32,6 +34,61 @@ function handleSymbol(symbol){
             break;
 
         case '←':
-            
+            if(buffer.length===1){
+                buffer='0';
+            }else{
+                buffer=buffer.substring(0,buffer.length-1);
+            }
+            break;
+
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;  
     }
 }
+
+function handleMath(symbol){
+    if(buffer==='0'){
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+    if(runningTotal===0){
+        runningTotal=intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator=symbol;
+    buffer='0';
+}
+
+function flushOperation(intBuffer){
+    if(previousOperator=== '+'){
+        runningTotal+=intBuffer;
+    }else if(previousOperator==='-'){
+        runningTotal-=intBuffer;
+    }else if(previousOperator==='×'){
+        runningTotal*=intBuffer;
+    }else{
+        runningTotal/=intBuffer;
+    }
+}
+
+function handleNumber(numberString){
+    if(buffer==='0'){
+        buffer=numberString;
+    }else{
+        buffer+=numberString;
+    }
+}
+
+function init(){
+    document.querySelector('.calc-buttons')
+    .addEventListener('click',function(event){
+        buttonClick(event.target.innerText);
+    });
+}
+
+init();
